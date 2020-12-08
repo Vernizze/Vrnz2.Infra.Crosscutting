7e681ccea86ec55ce43abb8ed54aa53a4451f300
Vernizze <cfvernizze@gmail.com>
2020-12-08 13:16:40 -0300

    Aumento de testes

---
+++ Vrnz2.Infra.Crosscutting.Test/Extensions/DateTimeExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vrnz2.Infra.CrossCutting.Extensions;
+using Xunit;
+
+namespace Vrnz2.Infra.Crosscutting.Test.Extensions
+{
+    public class DateTimeExtensionsTest
+    {
+        [Theory]
+        [InlineData("2020-12-18 00:00:00")]
+        public void FirstDayOfMonth_StringValue(string value)
+        {
+            var bla = value.FirstDayOfMonth();
+
+            //Assert.True(cpf.IsValid());
+        }
+    }
+}
