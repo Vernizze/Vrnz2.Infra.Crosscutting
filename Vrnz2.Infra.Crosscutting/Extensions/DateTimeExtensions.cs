041c939d14f0d4e7dd723aebca0dab7d419ad45c
Vernizze <cfvernizze@gmail.com>
2020-12-08 03:04:39 -0300

    Adicao de extensoes do tipo 'DateTime'

---
+++ Vrnz2.Infra.Crosscutting/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Vrnz2.Infra.Crosscutting.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FirstDayOfMonth(this DateTime value) 
+            => new DateTime(value.Year, value.Month, 1);
+
+        public static DateTime LastDayOfMonth(this DateTime value)
+            => FirstDayOfMonth(value).AddMonths(1).AddDays(-1);
+
+        public static DateTime? FirstDayOfMonth(this string value)
+        {
+            DateTime? result = null;
+
+            if (value.IsDate())
+                result = FirstDayOfMonth(DateTime.Parse(value));
+
+            return result;
+        }
+
+        public static DateTime? LastDayOfMonth(this string value)
+        {
+            DateTime? result = null;
+
+            if (value.IsDate())
+                result = LastDayOfMonth(DateTime.Parse(value));
+
+            return result;
+        }
+    }
+}
