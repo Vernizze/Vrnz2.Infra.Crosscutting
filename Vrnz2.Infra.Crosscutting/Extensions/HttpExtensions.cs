e789116351f9397265e06e4264f51301c8512db3
Vernizze <cfvernizze@gmail.com>
2020-12-27 22:07:25 -0300

    Inclusao de extensao de HttpStatusCode

---
+++ Vrnz2.Infra.Crosscutting/Extensions/HttpExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Vrnz2.Infra.CrossCutting.Extensions;
+
+namespace Vrnz2.Infra.Crosscutting.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static bool IsSuccessHttpStatusCode(this int? value)
+        {
+            if (value.IsNotNull())
+                return value.Value.IsSuccessHttpStatusCode();
+            else
+                return false;
+        }
+
+        public static bool IsSuccessHttpStatusCode(this int value) 
+            => value >= 200 && value < 300;
+
+        public static bool IsSuccessHttpStatusCode(this HttpStatusCode? value)
+        {
+            if (value.IsNotNull())
+                return value.Value.IsSuccessHttpStatusCode();
+            else
+                return false;
+        }
+
+        public static bool IsSuccessHttpStatusCode(this HttpStatusCode value)
+            => (int)value >= 200 && (int)value < 300;
+    }
+}
