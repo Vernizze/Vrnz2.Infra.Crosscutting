254e6b87541ca40974c8ea637e0bebb1a96f7c34
Vernizze <cfvernizze@gmail.com>
2021-01-03 07:16:54 -0300

    Implementacao do metodo de extensao de IEnumerable 'HaveAny' com predicado

--- Vrnz2.Infra.Crosscutting/Extensions/EnumerableExtensions.cs
+++ Vrnz2.Infra.Crosscutting/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Vrnz2.Infra.CrossCutting.Extensions
@@ -7,6 +8,9 @@ namespace Vrnz2.Infra.CrossCutting.Extensions
     {
         public static bool HaveAny<T>(this IEnumerable<T> value)
             => value.IsNotNull() && value.Any();
+
+        public static bool HaveAny<T>(this IEnumerable<T> value, Func<T, bool> predicate)
+            => value.IsNotNull() && value.Any(predicate);
 
         public static bool NotContains<T>(this IEnumerable<T> value, T item)
             => value.IsNotNull() && !value.Contains(item);
